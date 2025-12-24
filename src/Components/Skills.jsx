8d89140471ede60f73282cbf472b4314c5562563
jailanhammad <jailanhammad@gmail.com>
2025-12-25 01:38:35 +0200

    Home-Page-Api-Call-2

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useRef } from 'react';
 import './Skills.css';
 import SkillsC from './SkillsC';
-
-import ai from '../Assets/ai.svg';
-import figma from '../Assets/figma.svg';
-import ae from '../Assets/ae.svg';
-import blender from '../Assets/blender.svg';
-
-import ps from '../Assets/ps.svg';
-import pr from '../Assets/pr.svg';
-import vs from '../Assets/vs.svg';
-import ar from '../Assets/ar.svg';
-import lt from '../Assets/light.svg';
+import { supabase } from '../Supabase';
 
 const Skills = () => {
+
+
+    const containerRef = useRef(null);
+
+    const [loading, setLoading] = useState(true);
+    const [Skills, setSkills] = useState([
+
+
+        {
+
+                id:"",
+                title:"",
+                coverimg:"",
+                image:[],
+                description:"",
+                details:"",
+                category:[]
+                
+               } 
+
+
+]) ;
+
+
+useEffect(()=>{
+
+ async function getAllSkillsAPI(){
+  const res = await supabase.from("Skills").select("*");
+  setSkills(res.data);
+  // console.log(res.data);
+    setLoading(false);
+}
+getAllSkillsAPI();
+
+},[]);
+
+if (loading) return <p>Loading Api's...</p>;
+
     return (
         <>
         
@@ -22,44 +51,180 @@ const Skills = () => {
         <article class="skills-div">
 
     <section class="skills-section">
-        <h2 class="skills">My Skills</h2>
-        
+    {Skills
+  .filter(Skills => Skills.id === 1)
+  .map(Skills => (
+        <h2 class="skills">{Skills.title}</h2>
+) 
+) 
+}
         <div class="skills-grid">
-            <div class="skill-card">
-            <SkillsC ai={ai} aitext="Illustrator" aitextt="See projects" />    
-            </div>
+
+        {Skills
+  .filter(Skills => Skills.id === 1)
+  .map(Skills => (
 
             <div class="skill-card">
-            <SkillsC ai={figma} aitext="Figma" aitextt="See projects" />    
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
             </div>
 
-            <div class="skill-card">
-            <SkillsC ai={ae} aitext="After Effects" aitextt="See projects" />    
-            </div>
+) 
+) 
+}
+
+
+{Skills
+  .filter(Skills => Skills.id === 2)
+  .map(Skills => (
 
             <div class="skill-card">
-            <SkillsC ai={blender} aitext="Blender" aitextt="See projects" />    
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
             </div>
 
-            <div class="skill-card">
-            <SkillsC ai={ps} aitext="Photoshop" aitextt="See projects"/>    
-            </div>
+) 
+) 
+}
+
+
+{Skills
+  .filter(Skills => Skills.id === 3)
+  .map(Skills => (
 
             <div class="skill-card">
-            <SkillsC ai={pr} aitext="Premier" aitextt="See projects" />    
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
             </div>
 
-            <div class="skill-card">
-            <SkillsC ai={vs} aitext="VS Code" aitextt="See projects" />    
-            </div>
+) 
+) 
+}
+
+{Skills
+  .filter(Skills => Skills.id === 4)
+  .map(Skills => (
 
             <div class="skill-card">
-            <SkillsC ai={ar} aitext="Aero" aitextt="See projects" />    
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
             </div>
 
+) 
+) 
+}
+
+{Skills
+  .filter(Skills => Skills.id === 5)
+  .map(Skills => (
+
             <div class="skill-card">
-            <SkillsC ai={lt} aitext="Light Room" aitextt="See projects" />    
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
             </div>
+
+) 
+) 
+}
+
+
+{Skills
+  .filter(Skills => Skills.id === 6)
+  .map(Skills => (
+
+            <div class="skill-card">
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
+            </div>
+
+) 
+) 
+}
+
+
+{Skills
+  .filter(Skills => Skills.id === 7)
+  .map(Skills => (
+
+            <div class="skill-card">
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
+            </div>
+
+) 
+) 
+}
+
+{Skills
+  .filter(Skills => Skills.id === 8)
+  .map(Skills => (
+
+            <div class="skill-card">
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
+            </div>
+
+) 
+) 
+}
+
+{Skills
+  .filter(Skills => Skills.id === 9)
+  .map(Skills => (
+
+            <div class="skill-card">
+            <SkillsC
+             ai={Skills.img} 
+             aitext={Skills.text} 
+             aitextt={Skills.see} 
+             alt={Skills.alt} 
+
+              />    
+            </div>
+
+) 
+) 
+}
         </div>
     </section>
 
